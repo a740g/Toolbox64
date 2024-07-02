69fcf2ac2b660d59ce6c95b1e392691a44b04fe0
Samuel Gomes <samuel_gomes@outlook.com>
2024-07-02 16:19:20 +0530

    Make VSTi player downmix if there are more then 2 channels

--- libmidiplayer/foo_midi/OpalPlayer.h
+++ libmidiplayer/foo_midi/OpalPlayer.h
@@ -27,7 +27,7 @@ protected:
     virtual void SendSysEx(const uint8_t *event, size_t size, uint32_t portNumber) override;
 
 private:
-    static constexpr unsigned chipCount = 4; // each OPL3 chip has 18 voices, so this gives us a total of 72 voices
+    static constexpr unsigned chipCount = 6; // each OPL3 chip has 18 voices
 
     InstrumentBankManager *instrumentBankManager;
     OPLPlayer *synth;
