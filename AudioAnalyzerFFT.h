cd6767d190d95562976540b326e7d2f5f0657fcc
Samuel Gomes <samuel_gomes@outlook.com>
2024-12-30 17:41:49 +0530

    Cleanup AudioAnalyzerFFT

--- AudioAnalyzerFFT.h
+++ AudioAnalyzerFFT.h
@@ -15,8 +15,75 @@
 #include <cmath>
 #include <cstdlib>
 
+/// @brief FFT singleton class for audio spectrum analysis.
 class AudioAnalyzerFFT
 {
+public:
+    float DoFFT(uint16_t *amplitudeArray, const int16_t *sampleData, int sampleIncrement, int bitDepth)
+    {
+        const auto numSamples = std::min(1 << bitDepth, NUM_SAMPLES);
+        const auto halfNumSamples = numSamples >> 1;
+        auto averageIntensity = 0.0f;
+
+        for (auto i = 0; i < numSamples; ++i)
+        {
+            auto sample = float(*sampleData) * S16_TO_F32_MULTIPLIER;
+            fftBuffer[i][0] = int32_t(*sampleData) << 12;
+            fftBuffer[i][1] = 0;
+            averageIntensity += sample * sample;
+            sampleData += sampleIncrement;
+        }
+
+        averageIntensity = averageIntensity / float(numSamples);
+
+        PerformButterflyOperation(fftBuffer, bitDepth);
+
+        for (auto i = 1; i <= halfNumSamples; ++i)
+        {
+            auto realPart = fftBuffer[bitReversalTable[i] >> (FFT_POWER - bitDepth)][0] >> 12;
+            auto imagPart = fftBuffer[bitReversalTable[i] >> (FFT_POWER - bitDepth)][1] >> 12;
+            amplitudeArray[i - 1] = uint16_t(std::sqrt((realPart * realPart + imagPart * imagPart) * i));
+        }
+
+        return averageIntensity;
+    }
+
+    float DoFFT(uint16_t *amplitudeArray, const float *sampleData, int sampleIncrement, int bitDepth)
+    {
+        const auto numSamples = std::min(1 << bitDepth, NUM_SAMPLES);
+        const auto halfNumSamples = numSamples >> 1;
+        auto averageIntensity = 0.0f;
+
+        for (auto i = 0; i < numSamples; ++i)
+        {
+            auto sample = std::fmaxf(std::fminf(*sampleData, 1.0f), -1.0f);
+            fftBuffer[i][0] = int32_t(sample * F32_TO_S16_MULTIPLIER) << 12;
+            fftBuffer[i][1] = 0;
+            averageIntensity += sample * sample;
+            sampleData += sampleIncrement;
+        }
+
+        averageIntensity = averageIntensity / float(numSamples);
+
+        PerformButterflyOperation(fftBuffer, bitDepth);
+
+        for (auto i = 1; i <= halfNumSamples; ++i)
+        {
+            auto realPart = fftBuffer[bitReversalTable[i] >> (FFT_POWER - bitDepth)][0] >> 12;
+            auto imagPart = fftBuffer[bitReversalTable[i] >> (FFT_POWER - bitDepth)][1] >> 12;
+            amplitudeArray[i - 1] = uint16_t(std::sqrt((realPart * realPart + imagPart * imagPart) * i));
+        }
+
+        return averageIntensity;
+    }
+
+    static AudioAnalyzerFFT &Instance()
+    {
+        static AudioAnalyzerFFT instance;
+
+        return instance;
+    }
+
 private:
     static constexpr auto FFT_POWER = 11;
     static constexpr auto NUM_SAMPLES = 1 << FFT_POWER;
@@ -26,10 +93,42 @@ private:
     static constexpr auto S16_TO_F32_MULTIPLIER = 1.0f / 32768.0f;
     static constexpr auto F32_TO_S16_MULTIPLIER = 32767.0f;
 
-    static uint16_t bitReversalTable[NUM_SAMPLES];
-    static int32_t sinCosTable[HALF_SAMPLES][2];
+    AudioAnalyzerFFT()
+    {
+        for (auto i = 0; i < QUARTER_SAMPLES; i++)
+        {
+            auto angle = (2.0 * M_PI * i) / (4.0 * QUARTER_SAMPLES);
+            sinCosTable[i][0] = int32_t(std::cos(angle) * SCALE_FACTOR);
+            sinCosTable[i][1] = int32_t(std::sin(angle) * SCALE_FACTOR);
+        }
 
-    int32_t fftBuffer[NUM_SAMPLES][2];
+        auto reversedIndex = 0;
+        auto step = 0;
+
+        for (auto i = 0; i < NUM_SAMPLES; ++i)
+        {
+            bitReversalTable[i] = reversedIndex;
+            for (step = HALF_SAMPLES; step && (step <= reversedIndex); step >>= 1)
+                reversedIndex -= step;
+            reversedIndex += step;
+        }
+
+        for (auto i = HALF_SAMPLES / 4 + 1; i <= HALF_SAMPLES / 2; ++i)
+        {
+            sinCosTable[i][0] = sinCosTable[HALF_SAMPLES / 2 - i][1];
+            sinCosTable[i][1] = sinCosTable[HALF_SAMPLES / 2 - i][0];
+        }
+
+        for (auto i = HALF_SAMPLES / 2 + 1; i < HALF_SAMPLES; ++i)
+        {
+            sinCosTable[i][0] = -sinCosTable[HALF_SAMPLES - i][0];
+            sinCosTable[i][1] = sinCosTable[HALF_SAMPLES - i][1];
+        }
+    }
+
+    ~AudioAnalyzerFFT() = default;
+    AudioAnalyzerFFT(const AudioAnalyzerFFT &) = delete;
+    AudioAnalyzerFFT &operator=(const AudioAnalyzerFFT &) = delete;
 
     static inline constexpr auto MultiplyShift29(int32_t a, int32_t b)
     {
@@ -70,103 +169,11 @@ private:
         }
     }
 
-public:
-    AudioAnalyzerFFT()
-    {
-        for (auto i = 0; i < QUARTER_SAMPLES; i++)
-        {
-            auto angle = (2.0 * M_PI * i) / (4.0 * QUARTER_SAMPLES);
-            sinCosTable[i][0] = int32_t(std::cos(angle) * SCALE_FACTOR);
-            sinCosTable[i][1] = int32_t(std::sin(angle) * SCALE_FACTOR);
-        }
-
-        auto reversedIndex = 0;
-        auto step = 0;
-
-        for (auto i = 0; i < NUM_SAMPLES; ++i)
-        {
-            bitReversalTable[i] = reversedIndex;
-            for (step = HALF_SAMPLES; step && (step <= reversedIndex); step >>= 1)
-                reversedIndex -= step;
-            reversedIndex += step;
-        }
-
-        for (auto i = HALF_SAMPLES / 4 + 1; i <= HALF_SAMPLES / 2; ++i)
-        {
-            sinCosTable[i][0] = sinCosTable[HALF_SAMPLES / 2 - i][1];
-            sinCosTable[i][1] = sinCosTable[HALF_SAMPLES / 2 - i][0];
-        }
-
-        for (auto i = HALF_SAMPLES / 2 + 1; i < HALF_SAMPLES; ++i)
-        {
-            sinCosTable[i][0] = -sinCosTable[HALF_SAMPLES - i][0];
-            sinCosTable[i][1] = sinCosTable[HALF_SAMPLES - i][1];
-        }
-    }
-
-    auto DoFFT(uint16_t *amplitudeArray, const int16_t *sampleData, int sampleIncrement, int bitDepth)
-    {
-        const auto numSamples = std::min(1 << bitDepth, NUM_SAMPLES);
-        const auto halfNumSamples = numSamples >> 1;
-        auto averageIntensity = 0.0f;
-
-        for (auto i = 0; i < numSamples; ++i)
-        {
-            auto sample = float(*sampleData) * S16_TO_F32_MULTIPLIER;
-            fftBuffer[i][0] = int32_t(*sampleData) << 12;
-            fftBuffer[i][1] = 0;
-            averageIntensity += sample * sample;
-            sampleData += sampleIncrement;
-        }
-
-        averageIntensity = averageIntensity / float(numSamples);
-
-        PerformButterflyOperation(fftBuffer, bitDepth);
-
-        for (auto i = 1; i <= halfNumSamples; ++i)
-        {
-            auto realPart = fftBuffer[bitReversalTable[i] >> (FFT_POWER - bitDepth)][0] >> 12;
-            auto imagPart = fftBuffer[bitReversalTable[i] >> (FFT_POWER - bitDepth)][1] >> 12;
-            amplitudeArray[i - 1] = uint16_t(std::sqrt((realPart * realPart + imagPart * imagPart) * i));
-        }
-
-        return averageIntensity;
-    }
-
-    auto DoFFT(uint16_t *amplitudeArray, const float *sampleData, int sampleIncrement, int bitDepth)
-    {
-        const auto numSamples = std::min(1 << bitDepth, NUM_SAMPLES);
-        const auto halfNumSamples = numSamples >> 1;
-        auto averageIntensity = 0.0f;
-
-        for (auto i = 0; i < numSamples; ++i)
-        {
-            auto sample = std::fmaxf(std::fminf(*sampleData, 1.0f), -1.0f);
-            fftBuffer[i][0] = int32_t(sample * F32_TO_S16_MULTIPLIER) << 12;
-            fftBuffer[i][1] = 0;
-            averageIntensity += sample * sample;
-            sampleData += sampleIncrement;
-        }
-
-        averageIntensity = averageIntensity / float(numSamples);
-
-        PerformButterflyOperation(fftBuffer, bitDepth);
-
-        for (auto i = 1; i <= halfNumSamples; ++i)
-        {
-            auto realPart = fftBuffer[bitReversalTable[i] >> (FFT_POWER - bitDepth)][0] >> 12;
-            auto imagPart = fftBuffer[bitReversalTable[i] >> (FFT_POWER - bitDepth)][1] >> 12;
-            amplitudeArray[i - 1] = uint16_t(std::sqrt((realPart * realPart + imagPart * imagPart) * i));
-        }
-
-        return averageIntensity;
-    }
+    // Declare and define these inside the class.
+    static inline uint16_t bitReversalTable[NUM_SAMPLES];
+    static inline int32_t sinCosTable[HALF_SAMPLES][2];
+    static inline int32_t fftBuffer[NUM_SAMPLES][2];
 };
-
-uint16_t AudioAnalyzerFFT::bitReversalTable[AudioAnalyzerFFT::NUM_SAMPLES];
-int32_t AudioAnalyzerFFT::sinCosTable[AudioAnalyzerFFT::HALF_SAMPLES][2];
-
-static AudioAnalyzerFFT g_AudioAnalyzerFFT;
 
 /// @brief FFT for 16-bit integer samples. This computes the amplitude spectrum for the positive frequencies only.
 /// @param amplitudeArray The array where the resulting FFT amplitude data is stored.
@@ -176,7 +183,7 @@ static AudioAnalyzerFFT g_AudioAnalyzerFFT;
 /// @return Returns the average intensity level of the audio signal.
 auto AudioAnalyzerFFT_DoInteger(uint16_t *amplitudeArray, const int16_t *sampleData, int sampleIncrement, int bitDepth)
 {
-    return g_AudioAnalyzerFFT.DoFFT(amplitudeArray, sampleData, sampleIncrement, bitDepth);
+    return AudioAnalyzerFFT::Instance().DoFFT(amplitudeArray, sampleData, sampleIncrement, bitDepth);
 }
 
 /// @brief FFT for floating-point samples. This computes the amplitude spectrum for the positive frequencies only.
@@ -187,5 +194,5 @@ auto AudioAnalyzerFFT_DoInteger(uint16_t *amplitudeArray, const int16_t *sampleD
 /// @return Returns the average intensity level of the audio signal.
 auto AudioAnalyzerFFT_DoSingle(uint16_t *amplitudeArray, const float *sampleData, int sampleIncrement, int bitDepth)
 {
-    return g_AudioAnalyzerFFT.DoFFT(amplitudeArray, sampleData, sampleIncrement, bitDepth);
+    return AudioAnalyzerFFT::Instance().DoFFT(amplitudeArray, sampleData, sampleIncrement, bitDepth);
 }
